3c70112aff25efe437528ac2b31a684436320fb3
Akif <akifshixlarov@gmail.com>
2025-11-23 23:27:59 +0400

    done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """This module defines a Square class."""
 
-
 class Square:
     """Represents a square."""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
-
     def area(self):
-        return self.__size**2
-
-
+        """Returns the area of the square."""
+        return self.__size ** 2
 if __name__ == "__main__":
     my_square_1 = Square(3)
     print("Area: {}".format(my_square_1.area()))
@@ -33,4 +28,3 @@ if __name__ == "__main__":
 
     my_square_2 = Square(5)
     print("Area: {}".format(my_square_2.area()))
-    
